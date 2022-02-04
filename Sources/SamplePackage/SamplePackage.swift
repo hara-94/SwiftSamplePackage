7d8dbeb1d04017e6c52c5741d6e15235653cf92c
hara-94 <hikaru.h9894@gmail.com>
2022-02-04 13:38:01 +0900

    add: button

--- Sources/SamplePackage/SamplePackage.swift
+++ Sources/SamplePackage/SamplePackage.swift
@@ -1,6 +1,35 @@
+import UIKit
+
 public struct SamplePackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+}
+
+public class SamplePackageButton: UIButton {
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.bounds.width/2
+    }
+    
+    private func setupUI() {
+        backgroundColor = .init(red: 194/255, green: 230/255, blue: 255/255, alpha: 1)
+        titleLabel?.font = .boldSystemFont(ofSize: 18)
+        layer.borderColor = UIColor.lightGray.cgColor
+        layer.borderWidth = 2
+    }
+    
+    public func render(_ title: String?) {
+        self.setTitle(title, for: .normal)
     }
 }
